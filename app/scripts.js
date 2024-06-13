80f055f90c18f09532fb0ca30f6cb20a8dddfc4c
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2024-06-13 07:40:54 -0300

    pagina estática

--- app/scripts.js
+++ app/scripts.js
@@ -2,19 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
   const bannerContainer = document.querySelector('.banner-container');
   const banners = bannerContainer.querySelectorAll('.banner');
 
-  banners.forEach((banner, index) => {
-    showBanner(banner, index * 200);
+  // Configuração do IntersectionObserver para mostrar os banners
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Adiciona a classe 'show' com um atraso de 1 segundo
+        setTimeout(() => {
+          entry.target.classList.add('show');
+        }, 800); // 1000 milissegundos = 1 segundo
+      }
+    });
+  }, {
+    threshold: 0.5 // Define o threshold para 50% de visibilidade
   });
 
+  // Adiciona o observer a cada banner
+  banners.forEach(banner => {
+    observer.observe(banner);
+  });
+
+  // Event listener para o efeito do mouse (não modificado)
   document.addEventListener('mousemove', function(e) {
     const light = document.getElementById('light');
     light.style.left = `${e.clientX}px`;
     light.style.top = `${e.clientY}px`;
   });
 
-  function showBanner(banner, delay) {
-    setTimeout(() => {
-      banner.classList.add('show');
-    }, delay);
-  }
 });
